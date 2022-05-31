d27e27bcb7bb43cef004728474f4b5434a5e32d8
vnavesnoj <vnavesnoj@gmail.com>
2022-05-31 14:53:47 +0300

    Implement sub function

--- src/Launcher.java
+++ src/Launcher.java
@@ -7,9 +7,14 @@ public class Launcher {
         int a = 5;
         int b = 3;
         System.out.println(add(a, b));
+        System.out.println(sub(a, b));
     }
 
     private static int add(int a, int b) {
         return a + b;
+    }
+
+    private static int sub(int a, int b) {
+        return a - b;
     }
 }

07288f0b4970bfe2207d7615ce9d2e6971806f28
vnavesnoj <vnavesnoj@gmail.com>
2022-05-31 14:36:18 +0300

    Add git init

---
+++ src/Launcher.java
@@ -0,0 +1,9 @@
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public class Launcher {
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
